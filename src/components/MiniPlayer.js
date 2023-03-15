67382e1af7ce653fc1dcc1a76e89710d77d648ca
Faisal Ahmmed Tonmoy <42874834+faisaltonmoy@users.noreply.github.com>
2023-03-15 15:45:14 +0600

    progress_mini

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,14 +1,36 @@
+import { useRef, useState } from "react";
 import image from "../assets/images/3.jpg";
 import classes from "../styles/MiniPlayer.module.css";
 
-export default function MiniPlayer() {
+export default function MiniPlayer({ title, id }) {
+  const [status, setStatus] = useState(false);
+  const buttonRef = useRef();
+
+  //const videoUrl = `https://www.youtube.com/watch?v=${id}`;
+
+  function toggleMiniPlayer() {
+    if (!status) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setStatus(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setStatus(false);
+    }
+  }
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+    <div
+      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+      ref={buttonRef}
+      onClick={toggleMiniPlayer}
+    >
       <span className={`material-icons-outlined ${classes.open}`}>
         {" "}
         play_circle_filled{" "}
       </span>
-      <span className={`material-icons-outlined ${classes.close}`}>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleMiniPlayer}
+      >
         {" "}
         close{" "}
       </span>
--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -110,7 +110,7 @@ export default function Quiz() {
             submit={submit}
             progress={percentage}
           />
-          <MiniPlayer />
+          <MiniPlayer id={id} title={qna[currentQuestion].title} />
         </>
       )}
     </>
